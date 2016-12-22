12d8f4e140a74b5e296348e9a86cb0f132a518bb
ng700756@iteso.mx <ng700756@iteso.mx>
2016-12-21 23:51:16 -0600

    Support Class and ObjectProperties

--- src/main/java/sa/edu/kaust/ConverterController.java
+++ src/main/java/sa/edu/kaust/ConverterController.java
@@ -31,32 +31,14 @@ public class ConverterController {
 
 	public void convert(String endpoint, String query, String pattern, String refOnts) {
 
-		/*
-		query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
-				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
-				+ "PREFIX dcterms: <http://purl.org/dc/terms/>\n"
-				+ "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n"
-				+ "PREFIX sio: <http://semanticscience.org/resource/>\n"
-				+ "PREFIX ncit: <http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#>\n"
-				+ "SELECT DISTINCT ?disease ?phenotype WHERE {\n" + "?gda sio:SIO_000628 ?disease,?gene .\n"
-				+ "?disease rdf:type ncit:C7057 .\n" + "?disease skos:exactMatch ?test .\n"
-				+ "?test sio:SIO_001279 ?phenotype .\n"
-				+ "?disease dcterms:title ?diseaseName . FILTER (str(?diseaseName) = 'Obesity')\n" + "} limit 20";
-		pattern = "?disease SubClassOf(has-phenotype some ?phenotype)";
-*/
-	
-		 endpoint = "http://pro.knowitive.com/fuseki/sample/query";
-		 query =  "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
-				+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
-				+ "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n"
-				+ "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n"
-				+ "SELECT ?subject ?predicate ?object\n"
-				+ "WHERE {\n"
-				+ "?subject ?predicate ?object\n"
-				+ "}\n"
-				+ "LIMIT 25";
-			pattern = "?object SubClassOf(?subject some ?predicate)";
+		endpoint = "http://pro.knowitive.com/fuseki/Sample/query";
 
+		query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+				+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+				+ "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n" + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n"
+				+ "SELECT ?subject ?predicate ?object\n" + "WHERE {\n" + "?subject ?predicate ?object\n" + "}\n"
+				+ "LIMIT 25";
+		pattern = "?subject ?predicate ?object";
 
 		try {
 			// OutputStream os = response.getOutputStream();
--- src/main/java/sa/edu/kaust/Sparql2OWL.java
+++ src/main/java/sa/edu/kaust/Sparql2OWL.java
@@ -10,112 +10,127 @@ import org.apache.jena.query.*;
 
 import java.util.List;
 
-
-
 public class Sparql2OWL {
 
+	OWLOntology ontology;
+	OWLDataFactory dataFactory;
+	OWLOntologyManager manager;
+	
+	final String OWL_CLASS_TYPE = "rdf:type";
 
-    public void main(String[] args) throws OWLOntologyCreationException, OWLOntologyStorageException{
+	public void main(String[] args) throws OWLOntologyCreationException, OWLOntologyStorageException {
 
-        final  OWLDataFactory dataFactory = OWLManager.getOWLDataFactory();
-        final  OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        final  String iri = "www.somewhere.net/#";
-        final OWLOntology ontology = manager.createOntology();
+		final OWLDataFactory dataFactory = OWLManager.getOWLDataFactory();
+		final OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		final String iri = "www.somewhere.net/#";
+		final OWLOntology ontology = manager.createOntology();
 
+	}
 
-    }
+	public ResultSet getSparqlResults(String sparqlQuery, String sparqlEndpoint) {
 
-    public boolean checkInput(List<String> list, String[] strArr) {
-        int countVar = 0;
-        int countMatch = 0;
-        for (int i = 0; i < strArr.length; i++) {
-            if (strArr[i].startsWith("?")) {
-                countVar++;
-            }
-            if (list.contains(strArr[i].replace("?", ""))) {
-                countMatch++;
-            }
-        }
+		Query query = QueryFactory.create(sparqlQuery);
+		QueryExecution queryExec = QueryExecutionFactory.sparqlService(sparqlEndpoint, query);
+		ResultSet results = queryExec.execSelect();
+		ResultSet results2 = queryExec.execSelect();
+		ResultSetFormatter.out(results2);
 
-        if (countMatch == countVar && countVar <= list.size())
-            return true;
-        else
-            return false;
-    }
+		return results;
 
+	}
 
-    public ResultSet getSparqlResults(String sparqlQuery, String sparqlEndpoint) {
+	public OWLOntology createOntologyFromSparql(ResultSet results, String input, OWLOntology ontology,
+			OWLDataFactory dataFactory, OWLOntologyManager manager, String iri)
+			throws OWLOntologyStorageException, OWLOntologyCreationException, Exception {
 
-        Query query = QueryFactory.create(sparqlQuery);
-        QueryExecution queryExec = QueryExecutionFactory.sparqlService(sparqlEndpoint, query);
-        ResultSet results = queryExec.execSelect();
-        ResultSet results2 = queryExec.execSelect();
-        ResultSetFormatter.out(results2);
+		this.ontology = ontology;
+		this.dataFactory = dataFactory;
+		this.manager = manager;
 
-        return results;
+		List<String> classList = results.getResultVars();
 
-    }
+		String orig_input = new String(input);
+		String input1 = input.replaceAll("\\(", " ").replaceAll("\\)", " ");
+		System.out.println("input1:" + input1);
+		String[] strArr = input1.split(" ");
 
+		String domain = null;
+		String range = null;
+		String objectProperty = null;
 
-    public OWLOntology createOntologyFromSparql(ResultSet results, String input, OWLOntology ontology, OWLDataFactory dataFactory, OWLOntologyManager manager, String iri) {
+		while (results.hasNext()) {
 
+			Provider shortFormProvider = new Provider();
+			OWLEntityChecker entityChecker = new ShortFormEntityChecker(shortFormProvider);
+			QuerySolution querySolution = results.next();
 
-        List<String> classList = results.getResultVars();
-        System.out.println(classList.toString());
+			// input = input.replaceAll("\\?", "");
+			int ind1 = classList.indexOf(strArr[0].replace("?", ""));
+			int ind2 = classList.indexOf(strArr[1].replace("?", ""));
+			int ind3 = classList.indexOf(strArr[2].replace("?", ""));
+			String subject = querySolution.get(classList.get(ind1)).toString();
+			String predicade = querySolution.get(classList.get(ind2)).toString();
+			String object = querySolution.get(classList.get(ind3)).toString();
 
+			// System.out.println(dataFactory.getOWLClass(IRI.create(predicade)).toString());
 
-        String orig_input = new String(input);
-        String input1 = input.replaceAll("\\(", " ").replaceAll("\\)", " ");
-        System.out.println("input1:" + input1);
-        String[] strArr = input1.split(" ");
+			if (OWL_CLASS_TYPE.equals(dataFactory.getOWLClass(IRI.create(predicade)).toString())) {
+				if ("owl:Class".equals(dataFactory.getOWLClass(IRI.create(object)).toString())) {
+					addClass(subject);
+				}
+			}
 
-        System.out.println(checkInput(classList, strArr));
-        
-        //check #input and match with SPARQL query vars
-        if (checkInput(classList, strArr)) {
-            while (results.hasNext()) {
+			// ObjectProperty
 
+			if ("rdfs:domain".equals(dataFactory.getOWLClass(IRI.create(predicade)).toString())) {
+				domain = dataFactory.getOWLClass(IRI.create(object)).toString();
+			}
+			if ("rdfs:range".equals(dataFactory.getOWLClass(IRI.create(predicade)).toString())) {
+				range = dataFactory.getOWLClass(IRI.create(object)).toString();
+			}
+			if ("rdf:type".equals(dataFactory.getOWLClass(IRI.create(predicade)).toString())) {
+				if ("owl:ObjectProperty".equals(dataFactory.getOWLClass(IRI.create(object)).toString())) {
+					objectProperty = dataFactory.getOWLClass(IRI.create(subject)).toString();
+				}
+			}
 
-                Provider shortFormProvider = new Provider();
-                OWLEntityChecker entityChecker = new ShortFormEntityChecker(shortFormProvider);
+			// TODO check if all the are the components are the same objectProperty
+			if (objectProperty != null && domain != null && range != null) {
+				domain = domain.replace("<", "").replace(">", "");
+				range = range.replace("<", "").replace(">", "");
+				objectProperty = objectProperty.replace("<", "").replace(">", "");
 
-                QuerySolution querySolution = results.next();
-               
+				addObjectProperty(addClass(domain), addClass(range), objectProperty);
+				domain = objectProperty = range = null;
+			}
 
-                for (int i = 0; i < strArr.length; i++) {
-                    if (strArr[i].startsWith("?")) {
+			input = new String(orig_input);
+		}
 
-                        int ind = classList.indexOf(strArr[i].replace("?", ""));
-                        String str = querySolution.get(classList.get(ind)).toString();
-                        shortFormProvider.add(dataFactory.getOWLClass(IRI.create(str)));
-                        String[] srcArr = str.split("/");
-                        String var1 = srcArr[srcArr.length - 1];
-                        input = input.replace(strArr[i], var1);
+		return ontology;
+	}
 
-                    }
-                    if (strArr[i].startsWith("has")) {
-                        shortFormProvider.add(dataFactory.getOWLObjectProperty(IRI.create(iri + strArr[i])));
-                    }
-                }
+	public OWLClass addClass(String name) throws OWLOntologyStorageException, OWLOntologyCreationException, Exception {
+		IRI iri = IRI.create(name);
+		manager.setOntologyDocumentIRI(ontology, iri);
+		OWLClass owlClass = dataFactory.getOWLClass(IRI.create(name));
+		OWLAxiom axiom = dataFactory.getOWLDeclarationAxiom(owlClass);
+		manager.addAxiom(ontology, axiom);
+		return owlClass;
+	}
 
-                input = input.replaceAll("\\?", "");
+	public OWLObjectProperty addObjectProperty(OWLClass domain, OWLClass range, String name) {
 
-                ManchesterOWLSyntaxParser parser = OWLManager.createManchesterParser();
-                parser.setOWLEntityChecker(entityChecker);
-                parser.setStringToParse(input);
-                OWLAxiom axiom = parser.parseAxiom();
-                manager.addAxiom(ontology, axiom);
-                input = new String(orig_input);
-            }
+		OWLObjectProperty objectProperty = dataFactory.getOWLObjectProperty(IRI.create(name));
+		OWLObjectPropertyDomainAxiom domainAxiom = dataFactory.getOWLObjectPropertyDomainAxiom(objectProperty, domain);
+		OWLObjectPropertyRangeAxiom rangeAxiom = dataFactory.getOWLObjectPropertyRangeAxiom(objectProperty, range);
+		manager.addAxiom(ontology, domainAxiom);
+		manager.addAxiom(ontology, rangeAxiom);
+		return objectProperty;
+	}
 
-        } else {
-            System.out.println("wrong input");
-
-        }
-        return ontology;
-    }
-
-
-
+	public static String capitalize(String str) {
+		return str;
+	}
 
 }
